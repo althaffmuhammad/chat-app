05979f5890cadc67023e6ee847af55218d7f2c30
Muhammad Althaf <althafmuhammad250@gmail.com>
2024-03-26 10:56:12 +0530

    complete

--- client/src/Components/Sidebar/Conversation.jsx
+++ client/src/Components/Sidebar/Conversation.jsx
@@ -1,14 +1,18 @@
 import useConverstion from "../../zustand/useConversation"
+import { useSocketContext } from './../../context/SocketContext';
 
 
 const Conversation = ({conversation,lastIdx,emoji}) => {
 	const {selectedConversation,setSelectedCovnersation}=useConverstion()
 	const isSelected=selectedConversation?._id===conversation._id
+
+	const { onlineUsers } = useSocketContext();
+	const isOnline = onlineUsers.includes(conversation._id);
   return (
 		<>
 			<div className={`flex gap-2 items-center hover:bg-sky-500 rounded p-2 py-1 cursor-pointer
 				${isSelected? "bg-sky-500":""}`} onClick={()=>setSelectedCovnersation(conversation)}>
-				<div className='avatar online'>
+				<div className={`avatar ${isOnline ? "online" : ""}`}>
 					<div className='w-12 rounded-full'>
 						<img
 							src={conversation.profilePic}
--- server/server.js
+++ server/server.js
@@ -2,22 +2,20 @@ import express from 'express';
 import 'colors';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
-import cors from 'cors';
 
 import authRoutes from './routes/auth.route.js';
 import messageRoutes from './routes/message.route.js';
 import userRoutes from './routes/user.route.js';
 
 import connectToMongoDB from './db/connectToMongoDB.js';
+import {app, server} from './socket/socket.js';
 
-const app = express ();
 const PORT = process.env.PORT || 5000;
 
 dotenv.config ();
 
 //middle
 app.use (express.json ());
-app.use (cors ());
 app.use (cookieParser ());
 
 //database config
@@ -34,6 +32,6 @@ app.get ('/', (req, res) => {
 });
 
 //port
-app.listen (PORT, () =>
+server.listen (PORT, () =>
   console.log (`server running on port ${PORT}`.bgGreen.black)
 );
