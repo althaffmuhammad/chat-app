4d0c7efe4c55e0fcf34e7a8ce3288bb958b36f69
Muhammad Althaf <althafmuhammad250@gmail.com>
2024-04-25 19:59:51 +0530

    socket updated

--- client/src/context/SocketContext.jsx
+++ client/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("https://chat-app-ax6v.onrender.com", {
+			const socket = io("https://chat-app-messenger-w1ix.onrender.com", {
 				query: {
 					userId: authUser._id,
 				},
